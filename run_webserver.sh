26ece76870316cce8459386118ff943de94563bf
David Aparicio <david.aparicio@free.fr>
2023-04-10 18:07:36 +0200

    ✨ feat: run local web server (via Python)

---
+++ run_webserver.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "-h" ]; then #https://stackoverflow.com/a/30830291
+  echo "Usage: $(basename "$0") [-b/--build]"
+  exit 0
+fi
+
+if [ "$1" != "-h" ]; then
+  echo "==== BUILD CODELAB ===="
+  if ! command -v claat &> /dev/null
+  then
+    echo "claat required but it's not installed. Skipping."
+    echo "More information: https://github.com/googlecodelabs/tools/blob/main/claat/README.md"
+    # go install github.com/googlecodelabs/tools/claat@latest
+    exit
+  else
+    claat export *
+  fi
+fi
+
+echo "====  RUN CODELAB  ===="
+#https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server
+#https://stackoverflow.com/questions/38485373/from-the-terminal-verify-if-python-3-is-installed
+if ! command -v python3 >/dev/null 2>&1
+then
+  python3 -m http.server 8000
+else
+  python -m SimpleHTTPServer 8000
+fi
